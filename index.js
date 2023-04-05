af1b083550cb073dd773ead9b285cd759ccc4c32
JoseLuisColcha <jose.colcha@epn.edu.ec>
2023-04-04 21:24:02 -0500

    First configurations

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+
+console.log(process.env);
+
+//Crear servidor de express
+
+const app = express();
+
+//Directorio público
+
+app.use(express.static("public"));
+
+//Rutas
+app.use("/api/auth", require("./routes/auth"));
+
+//TODO: CRUD: Eventos
+
+//Escuchar peticiones
+
+app.listen(process.env.PORT, () =>
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
+);
