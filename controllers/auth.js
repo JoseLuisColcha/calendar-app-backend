cd1df29cd6eb721649c630ba83975b6f0f1755e2
JoseLuisColcha <jose.colcha@epn.edu.ec>
2023-04-04 22:39:52 -0500

    Add feature to get post information

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,36 @@
 const { response } = require("express");
 
-const createUser = (req, res = express.response) => {
+const createUser = (req, res = response) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: "El nombre debe ser de 5 letras",
+    });
+  }
+
   res.json({
     ok: true,
     msg: "register",
+    name,
+    email,
+    password,
   });
 };
 
-const loginUser = (req, res) => {
+const loginUser = (req, res = response) => {
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
     msg: "login",
+    email,
+    password,
   });
 };
 
-const revalidarToken = (req, res) => {
+const revalidateToken = (req, res = response) => {
   res.json({
     ok: true,
     msg: "renewar token",
@@ -23,5 +39,5 @@ const revalidarToken = (req, res) => {
 module.exports = {
   createUser,
   loginUser,
-  revalidarToken,
+  revalidateToken,
 };
--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const app = express();
 
 app.use(express.static("public"));
 
+// Lectura y parseo del body
+app.use(express.json());
+
 //Rutas
 app.use("/api/auth", require("./routes/auth"));
 
