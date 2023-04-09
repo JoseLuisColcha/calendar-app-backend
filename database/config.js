35632417e8fe804e398dc6ec5f279b8e21ad3d89
JoseLuisColcha <jose.colcha@epn.edu.ec>
2023-04-08 20:50:29 -0500

    Create configuration to connect with mongoDB

---
+++ database/config.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CNN);
+
+    console.log("db connection established");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Couldn't connect to Mongo");
+  }
+};
+
+module.exports = { dbConnection };
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { dbConnection } = require("./database/config");
 require("dotenv").config();
 
 console.log(process.env);
@@ -6,6 +7,9 @@ console.log(process.env);
 //Crear servidor de express
 
 const app = express();
+
+//Basde de datos
+dbConnection();
 
 //Directorio público
 
