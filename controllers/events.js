8ea615c6c4d094a28df5a57ac888ad2459f15d71
JoseLuisColcha <jose.colcha@epn.edu.ec>
2023-04-09 21:47:52 -0500

    Add function to delete event

--- controllers/events.js
+++ controllers/events.js
@@ -37,7 +37,7 @@ const updateEvent = async (req, res = response) => {
     const event = await Event.findById(eventId);
 
     if (!event) {
-      res.status(404).json({
+      return res.status(404).json({
         ok: false,
         msg: "Event not found",
       });
@@ -72,11 +72,38 @@ const updateEvent = async (req, res = response) => {
   }
 };
 
-const deleteEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "Delete event",
-  });
+const deleteEvent = async (req, res = response) => {
+  const eventId = req.params.id;
+  const uid = req.uid;
+  try {
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Event not found",
+      });
+    }
+
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "Not authorized to delete this event",
+      });
+    }
+
+    await Event.findByIdAndDelete(eventId);
+
+    res.json({
+      ok: true,
+    });
+  } catch (error) {
+    console.log(error);
+    request.status(500).json({
+      ok: false,
+      msg: "Talk to the admin",
+    });
+  }
 };
 
 module.exports = {
